31ce0689c89990483090a44e3048ff2119dd0025
abdul1ah <almamun01817@gmail.com>
2024-06-14 12:40:41 +0600

    end

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { sendResponse } from './app/utils/sendResponse';
+import httpStatus from 'http-status';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.get('/ha', (req, res) => {
-      res.send('Car Rental Reservation System');
+    app.use('/api/jj', (req, res) => {
+      sendResponse(res,{
+        statusCode:httpStatus.OK,
+        message:'jjj',
+        data:null,
+        success:true
+       })
     });
     app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
